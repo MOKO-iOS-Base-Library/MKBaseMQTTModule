201d0602cdaa3db54a2ba7aca83b4151c00049a5
lovexiaoxia <aadyx2007@163.com>
2021-08-23 11:28:26 +0800

    Initial commit

---
+++ Example/MKBaseMQTTModule/MKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MKAppDelegate.h
+//  MKBaseMQTTModule
+//
+//  Created by aadyx2007@163.com on 08/23/2021.
+//  Copyright (c) 2021 aadyx2007@163.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
